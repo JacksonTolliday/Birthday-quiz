5bd0f4fb150942bc31e101525f66f9871c4c099e
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-09-11 11:56:01 -0400

    Updated from Brython Server: 9/11/2018 11:56:00 AM

--- birthday.py
+++ birthday.py
@@ -31,13 +31,45 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+import datetime from datetime
 
 name = input("Hello, what is your name? ")
 month = input("Hi " + name + ", what was the name of the month you were born in? ")
-year = input("And what year were you born in, " + name + "? ")
-day = input("And the day? ")
+year = int(input("And what year were you born in, " + name + "? "))
+day = int(input("And the day? "))
 
 monthlist = ['December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November']
 winter = monthlist[:3]
-spring = monthlist[3:5]
-summer = monthlist[]
+spring = monthlist[3:6]
+summer = monthlist[6:9]
+fall = monthlist[9:]
+
+seasonprint = '0'
+
+if month in winter:
+    seasonprint = 'winter'
+elif month in spring:
+    seasonprint = 'spring'
+elif month in summer:
+    seasonprint = 'summer'
+elif month in fall:
+    seasonprint = 'two thousands'
+else:
+    seasonprint = '0'
+
+yearprint = "0"
+
+if year < 1980:
+    yearprint = 'Stone Age'
+elif year >= 1980:
+    if year <= 1989:
+        yearprint = 'eighties'
+    elif year >= 1990:
+        if year <= 1999:
+            yearprint = 'nineties'
+        else: # the only combination left is two thousands
+            yearprint = 'two thousands'
+if month == October and day == 31:
+    print("You were born on Halloween!")
+elif 
+print("" + str(name) + ", you are a " + str(seasonprint) + " baby of the " + str(yearprint) + ".")
